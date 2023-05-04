ef4fa94c68bd1acf4dbc650d5a4517ddb0482415
Chris Farris <chris.farris@playonsports.com>
2023-05-04 16:28:55 -0400

    Enabling Macie in the Org Management Account

--- scripts/enable_macie_delegation.sh
+++ scripts/enable_macie_delegation.sh
@@ -13,4 +13,5 @@ REGIONS=`aws ec2 describe-regions --query 'Regions[].[RegionName]' --output text
 for r in $REGIONS ; do
   echo "Enabling Macie Delegated Admin in $r"
   aws macie2 enable-organization-admin-account --admin-account-id $MACIE_ACCOUNT --region $r
+  aws macie2 enable-macie  --region $r
 done
