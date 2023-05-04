4820ae388951723a1f9e2a7da74871dbaee2fbfb
Chris Farris <chris.farris@playonsports.com>
2023-05-04 16:34:09 -0400

    Add new scripts for macie setup

---
+++ scripts/disabled_automated_discovery.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Script to enable Delegated Admin in a payer account for all Macie Regions
+
+REGIONS=`aws ec2 describe-regions --query 'Regions[].[RegionName]' --output text`
+for r in $REGIONS ; do
+  echo "Disabling Macie Automated Discovery in $r"
+  aws macie2 update-automated-discovery-configuratione  --region $r --status DISABLED
+done
